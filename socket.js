107f77fb2da179bb5a861864a8bcb9ffcaa0874c
hoaihv-nexlesoft <hoaihv@nexlesoft.com>
2026-07-13 16:27:31 +0700

    update socket

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const accessToken = localStorage.getItem("token");
 
-export const socket = io("https://cms.trunggian.io.vn", {
+export const socket = io("https://api.trunggian.io.vn", {
   transports: ["websocket"],
   auth: {
     token: accessToken,
